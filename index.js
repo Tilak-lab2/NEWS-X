5362a0741e3bb497cb4b8b0896a4f81313f532f6
tilak lab-2 <sritilak007@gmail.com>
2022-01-15 17:27:13 +0530

    Added flash mongostore and eexpress session

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const app=express()
 const passport=require('passport')
 const session=require('express-session')
 const passportLocal=require("./config/passport")
+const MongoStore=require('connect-mongo')(session)
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname ,'./views'))
 
-pp.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))
 app.use(expressLayouts);
 app.use(session({
     secret:process.env.SESSION_COOKIE,
@@ -33,9 +37,14 @@ app.use(express.static('./assets'))
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 // For authentication passport
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
+app.use(flash())
+// to use flash as middleware
+app.use(customMware.setFlash);
 
 
 
