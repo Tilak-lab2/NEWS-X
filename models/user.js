aca7b9cf0cd9a81a7b7185da52d290656d3a442d
tilak lab-2 <sritilak007@gmail.com>
2022-01-17 09:54:16 +0530

    Added Multer

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
-
-
+const path=require('path')
+const multer=require('multer')
+const AVATAR_PATH=path.join('/uploads/avatars')
 
 const userSchema=new mongoose.Schema({
     email:{
@@ -16,9 +17,38 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true,
     },
+    avatar:{
+        type:String,
+    }
 },{
     timestamps:true
 })
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,"..",AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+  userSchema.statics.UploadedAvatar=multer({
+    storage,
+    fileFilter:(req,file,cb)=>{
+        if(
+            file.mimetype=='image/png' ||
+            file.mimetype=='image/jpg' || 
+            file.mimetype=='image/jpeg'
+        ){
+            cb(null,true)
+        }else{
+            cb(null,false)
+             return cb(new Error("Sorry You cannot Upload it , Only png,jpg,jpeg"))
+           
+        }
+    },
+    
+
+}).single('avatar')
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
